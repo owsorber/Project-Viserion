6ba2d660293543ba0a7565d528e4550394d65cb1
Owen Sorber <ons3@cornell.edu>
2023-11-21 18:05:56 -0500

    Update simulation with mdp data collector

--- simulation/simulate.py
+++ simulation/simulate.py
@@ -29,6 +29,9 @@ class FullIntegratedSim:
     self.display_graphics = display_graphics
     self.sim_frequency_hz = sim_frequency_hz
     self.airsim_frequency_hz = airsim_frequency_hz
+
+    # For data collection
+    self.mdp_data_collector = mdp.MDPDataCollector(self, mdp.bb_reward, self.sim_time * self.sim_frequency_hz)
     
     # Currently unused, but could be used for how often the agent selects a new action
     self.agent_interaction_frequency = agent_interaction_frequency
@@ -36,15 +39,26 @@ class FullIntegratedSim:
     # Triggered when sim is complete
     self.done: bool = False
   
-  def simulation_loop(self):
-    """
+  """
     Run loop for one simulation.
-    """
+  """
+  def simulation_loop(self):
     update_num = int(self.sim_time * self.sim_frequency_hz)  # how many simulation steps to update the simulation
     relative_update = self.airsim_frequency_hz / self.sim_frequency_hz  # rate between airsim and JSBSim
     graphic_update = 0
 
-    for i in range(update_num):
+    i = 0
+    while i < update_num:
+      # Do autopilot controls
+      state, action, log_prob = mdp.enact_autopilot(self.sim, self.autopilot)
+
+      # Run another sim step
+      self.sim.run()
+
+      # Increment timestep
+      i += 1
+
+      # Airsim update
       graphic_i = relative_update * i
       graphic_update_old = graphic_update
       graphic_update = graphic_i // 1.0
@@ -55,13 +69,19 @@ class FullIntegratedSim:
       if self.sim.client.simGetCollisionInfo().has_collided:
         print('Aircraft has collided.')
         self.done = True
+
+      # Get new state
+      next_state = mdp.state_from_sim(self.sim)
+
+      # Data collection update for this step
+      self.mdp_data_collector.update(i-1, state, action, log_prob, next_state, self.done)
+
+      # End if collided
+      if self.done == True:
         break
-      
-      # Do autopilot controls and run the simulation
-      mdp.enact_autopilot(self.sim, self.autopilot)
-      self.sim.run()
     
     self.done = True
+    self.mdp_data_collector.terminate(i)
     print('Simulation complete.')
 
 if __name__ == "__main__":
